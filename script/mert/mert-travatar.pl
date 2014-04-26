08dde44e33e1961809158ec75a089fc92fb953ec
Graham Neubig <neubig@gmail.com>
2014-04-26 21:27:09 +0200

    Fix in tuning over differnt factors

--- script/mert/mert-travatar.pl
+++ script/mert/mert-travatar.pl
@@ -115,13 +115,13 @@ foreach $iter (1 .. $MAX_ITERS) {
         if($CAND_TYPE eq "nbest") {
             my $nbests = join(" ", map { "$WORKING_DIR/run$_.uniq" } (1 .. $iter-1));
             safesystem("$TRAVATAR_DIR/script/mert/nbest-uniq.pl $nbests < $WORKING_DIR/run$iter.nbest > $WORKING_DIR/run$iter.uniq");
-            safesystem("$TRAVATAR_DIR/src/bin/batch-tune -tune-factor $TUNE_FACTOR -threads $THREADS -nbest $WORKING_DIR/run$iter.uniq -stat_out $WORKING_DIR/run$iter.stats -eval \"$EVAL\" $REF 2> $prev.stats.log") or die "batch-tune stats extraction failed";
+            safesystem("$TRAVATAR_DIR/src/bin/batch-tune -tune_factor $TUNE_FACTOR -trg_factors $TRG_FACTORS -threads $THREADS -nbest $WORKING_DIR/run$iter.uniq -stat_out $WORKING_DIR/run$iter.stats -eval \"$EVAL\" $REF 2> $prev.stats.log") or die "batch-tune stats extraction failed";
             $nbests = join(",", map { "$WORKING_DIR/run$_.uniq" } (1 .. $iter));
             my $stats = join(",", map { "$WORKING_DIR/run$_.stats" } (1 .. $iter));
             safesystem("$TRAVATAR_DIR/src/bin/batch-tune -threads $THREADS -nbest $nbests -stat_in $stats -eval \"$EVAL\" -weight_in $prev.weights $TUNE_OPTIONS $REF > $next.weights 2> $prev.tune.log") or die "batch-tune failed";
         } elsif($CAND_TYPE eq "forest") {
             my $forests = join(",", map { "$WORKING_DIR/run$_.forest" } (1 .. $iter));
-            safesystem("$TRAVATAR_DIR/src/bin/batch-tune -tune-factor $TUNE_FACTOR -threads $THREADS -forest $forests -eval \"$EVAL\" -weight_in $prev.weights $TUNE_OPTIONS $REF > $next.weights 2> $prev.tune.log") or die "batch-tune failed";
+            safesystem("$TRAVATAR_DIR/src/bin/batch-tune -tune_factor $TUNE_FACTOR -trg_factors $TRG_FACTORS -threads $THREADS -forest $forests -eval \"$EVAL\" -weight_in $prev.weights $TUNE_OPTIONS $REF > $next.weights 2> $prev.tune.log") or die "batch-tune failed";
         } elsif ($CAND_TYPE eq "nbestgeo") {
             my $nbests = join(" ", map { "$WORKING_DIR/run$_.uniq" } (1 .. $iter-1));
             safesystem("$TRAVATAR_DIR/script/mert/nbest-uniq.pl $nbests < $WORKING_DIR/run$iter.nbest > $WORKING_DIR/run$iter.uniq");
