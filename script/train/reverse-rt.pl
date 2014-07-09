4d381203646711075ac9d34d7974608ab6514868
Graham Neubig <neubig@gmail.com>
2014-07-09 22:21:14 +0900

    Fixed reverse

--- script/train/reverse-rt.pl
+++ script/train/reverse-rt.pl
@@ -16,7 +16,12 @@ if(@ARGV != 0) {
 while(<STDIN>) {
     chomp;
     my @arr = split(/ \|\|\| /, $_, -1);
-    die "Wrong number of columns (must be >= 3):\n$_" if (@arr < 3);
+    die "Wrong number of columns (must >= 4):\n$_" if (@arr < 4);
     my $tmp = $arr[0]; $arr[0] = $arr[1]; $arr[1] = $tmp;
-    print join(" ||| ", @arr)."\n";
+    my $align = pop @arr;
+    my @out;
+    while($align =~ /(\d+)-(\d+)/g) { 
+        push @out, "$2-$1";
+    }
+    print join(" ||| ", @arr, "@out")."\n";
 }
