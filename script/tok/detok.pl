fa5b4d2bf911ae47ff204a2ec7cf34217175136a
Graham Neubig <neubig@gmail.com>
2014-07-19 01:00:13 +0900

    Better detokenization of scientific units

--- script/tok/detok.pl
+++ script/tok/detok.pl
@@ -21,7 +21,8 @@ if(@ARGV != 0) {
 while(<STDIN>) {
     chomp;
     # Units
-    s/([0-9]) (℃|w|t|°) /$1$2 /g;
+    s/([0-9]) (|T|G|M|k|h|da|d|c|m|μ|n|p)(m|g|l|s|A|K|mol|cd|rad|sr|Hz|N|Pa|J|W|C|V|eV|F|Ω|S|Wb|T|H|lm|lx|Bq|Gy|Sv|kat|rpm|Wd|dB|db)( |\/|-)/$1$2$3$4/g;
+    s/([0-9]) (℃|°|K) /$1$2 /g;
     s/(\p{InHiragana}|\p{InKatakana}|\p{InHalfwidthAndFullwidthForms}|\p{InCJKUnifiedIdeographs}|[、。「」　“”]) (\p{InHiragana}|\p{InKatakana}|\p{InHalfwidthAndFullwidthForms}|\p{InCJKUnifiedIdeographs}|[、。「」　“”])/$1$2/g;
     s/(\p{InHiragana}|\p{InKatakana}|\p{InHalfwidthAndFullwidthForms}|\p{InCJKUnifiedIdeographs}|[、。「」　“”]) (\p{InHiragana}|\p{InKatakana}|\p{InHalfwidthAndFullwidthForms}|\p{InCJKUnifiedIdeographs}|[、。「」　“”])/$1$2/g;
     s/ ([,\.\?:。、%])/$1/g;
@@ -30,12 +31,12 @@ while(<STDIN>) {
     s/`` /"/g;
     s/ ''/"/g;
     s/ (n't|'ve|'s|'d|'m|'ll)/$1/gi;
-    s/ -rrb-/)/gi;
-    s/-lrb- /(/gi;
-    s/ -rsb-/]/gi;
-    s/-lsb- /[/gi;
-    s/ -rcb-/}/gi;
-    s/-lcb- /{/gi;
+    s/ ?-rrb-/)/gi;
+    s/-lrb- ?/(/gi;
+    s/ ?-rsb-/]/gi;
+    s/-lsb- ?/[/gi;
+    s/ ?-rcb-/}/gi;
+    s/-lcb- ?/{/gi;
     if($LANG !~ /^(zh|ja)$/) {
         $_ = ucfirst($_);
     }
