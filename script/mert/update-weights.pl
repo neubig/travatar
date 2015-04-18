neubig/travatar

commit 89a35b68a890f9838fde0180e34baf29d027f1b3

Fixed breaking when all weights are zero

script/mert/update-weights.pl
@@ -71,7 +71,9 @@ while(<FILE0>) {
     last if not $_;
     my ($oldname, $oldweight) = split(/=/);
     my $newweight;
-    if(!%wmap) {
+    if(not @weights) {
+        print "$oldname=0\n";
+    } elsif(!%wmap) {
         die "Weight sizes don't match" if not @weights;
         my $newname = shift(@names);
         $newweight = shift(@weights);

src/include/travatar/lookup-table-fsm.h
@@ -135,7 +135,8 @@ public:
     void SetTrgFactors(const int trg_factors) { trg_factors_ = trg_factors; } 
     void SetSaveSrcStr(const bool save_src_str);
 
-    static TranslationRuleHiero* GetUnknownRule(WordId unknown_word, const HieroHeadLabels& head_labels);
+    static TranslationRuleHiero* GetUnknownRule(const WordId unknown_word, const HieroHeadLabels& head_labels);
+    static TranslationRuleHiero* GetUnknownRule(const WordId src, WordId unknown_word, const HieroHeadLabels& head_labels);
 
     static LookupTableFSM * ReadFromFiles(const std::vector<std::string> & filenames);
 

src/lib/lookup-table-fsm.cc
@@ -145,10 +145,8 @@ RuleFSM * RuleFSM::ReadFromRuleTable(istream & in) {
     return ret;
 }
 
-#define VALID_NODE 99999999
-
 HyperGraph * LookupTableFSM::TransformGraph(const HyperGraph & graph) const {
-    
+    int VALID_NODE = 9999999; 
     HyperGraph* _graph = new HyperGraph;
     Sentence sent = graph.GetWords();
     _graph->SetWords(sent);
@@ -174,7 +172,7 @@ HyperGraph * LookupTableFSM::TransformGraph(const HyperGraph & graph) const {
             if (node_span.second - node_span.first == 1) {
                 int i = node_span.first;
                 if(node->GetEdges().size() == 0) {
-                    TranslationRuleHiero* unk_rule = GetUnknownRule(delete_unknown_? Dict::WID("") : sent[i], head_node.first);
+                    TranslationRuleHiero* unk_rule = GetUnknownRule(sent[i], delete_unknown_? Dict::WID("") : sent[i], head_node.first);
                     HyperEdge* unk_edge = LookupTableFSM::TransformRuleIntoEdge(node_map,i,i+1,temp_spans,unk_rule,save_src_str_);
                     edge_list.push_back(unk_edge);
                     delete unk_rule;
@@ -454,7 +452,11 @@ HyperNode* LookupTableFSM::FindNode(HieroNodeMap& map_ptr,
     }
 }
 
-TranslationRuleHiero* LookupTableFSM::GetUnknownRule(WordId unknown_word, const HieroHeadLabels& head_labels) 
+TranslationRuleHiero* LookupTableFSM::GetUnknownRule(WordId unknown_word, const HieroHeadLabels& head_labels) {
+    return GetUnknownRule(unknown_word,unknown_word, head_labels);
+}
+
+TranslationRuleHiero* LookupTableFSM::GetUnknownRule(WordId src, WordId unknown_word, const HieroHeadLabels& head_labels) 
 {
     CfgDataVector target;
     for (int i=1; i < (int)head_labels.size(); ++i) 
@@ -462,7 +464,7 @@ TranslationRuleHiero* LookupTableFSM::GetUnknownRule(WordId unknown_word, const 
     return new TranslationRuleHiero(
         target,
         Dict::ParseSparseVector("unk=1"),
-        CfgData(Sentence(1, unknown_word), head_labels[0])
+        CfgData(Sentence(1, src), head_labels[0])
     );
 }
 

commit 44bb5f584770e5ef14d7ab42a86c836f99cfef3f

Fixed bug caused by commit: 89a35b68a890f9838fde0180e34baf29d027f1b3

script/mert/update-weights.pl
@@ -71,9 +71,7 @@ while(<FILE0>) {
     last if not $_;
     my ($oldname, $oldweight) = split(/=/);
     my $newweight;
-    if(not @weights) {
-        print "$oldname=0\n";
-    } elsif(!%wmap) {
+    if(!%wmap) {
         die "Weight sizes don't match" if not @weights;
         my $newname = shift(@names);
         $newweight = shift(@weights);